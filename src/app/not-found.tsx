81ae02c2c7fbc945de55eb156b4877a179ada1e6
Daniel Riêgo <danielriegodevjobs#@gmail.com>
2026-01-03 16:07:00 -0300

    Update até aula 138

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,24 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-80 bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center'
-      )}
-    >
-      <div>
-        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-        <p>Página não encontrada</p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center'
+        )}
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+          <div>
+            Erro 404 - A página que você está tentando acessar não existe neste
+            site.
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -37,7 +37,6 @@ export class JsonPostRepository implements PostRepository {
     return posts.filter((post) => post.published);
   }
   async findById(id: string): Promise<PostModel> {
-    await this.simulateWait();
     const posts = await this.findAllPublic();
     const post = posts.find((post) => post.id === id);
 
@@ -46,7 +45,6 @@ export class JsonPostRepository implements PostRepository {
     return post;
   }
   async findBySlug(slug: string): Promise<PostModel> {
-    await this.simulateWait();
     const posts = await this.findAllPublic();
     const post = posts.find((post) => post.slug === slug);
 
